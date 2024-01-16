5564a70b850ec507bbe398e92569343901b9e625
JCYhapon <yhaponjc@gmail.com>
2024-01-16 15:22:03 +0800

    Version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         secondary: "#00f6ff",
         dimWhite: "rgba(255, 255, 255, 0.7)",
         dimBlue: "rgba (9, 151, 124, 0.1)",
-        sample: "1E9223",
+        newGreen: "1E9223",
       },
     },
     screens: {
