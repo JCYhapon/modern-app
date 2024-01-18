b9143f24ace2a85301ffa71a41bb088bf719eabe
JCYhapon <yhaponjc@gmail.com>
2024-01-18 22:19:36 +0800

    responsive in nvabar, also adding toggle close and menu function

--- modern_app/src/components/Navbar.jsx
+++ modern_app/src/components/Navbar.jsx
@@ -5,6 +5,8 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank logo" className="w-[124px] h-[32px]" />
@@ -14,13 +16,23 @@ const Navbar = () => {
           <li
             key={nav.id}
             className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              index === navLinks.lenght - 1 ? "mr-0" : "mr-10"
-            } text-white mr-10`}
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
         ))}
       </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div className={`${toggle ? "flex" : "hidden"}`}></div>
+      </div>
     </nav>
   );
 };
