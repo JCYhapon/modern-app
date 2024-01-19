fe70fb3ae53ec83762e76d333efbf93199e0d0d3
JCYhapon <yhaponjc@gmail.com>
2024-01-19 20:50:03 +0800

    Updated Hero Section: adding responsiveness

--- modern_app/src/components/Hero.jsx
+++ modern_app/src/components/Hero.jsx
@@ -9,10 +9,18 @@ const Hero = () => (
     >
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-none-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-        <p className={`${styles.paragraph}`}>
+        <p className={`${styles.paragraph} ml-2`}>
           <span className="text-white">20% </span>Discount For{" "}
-          <span className="text-white">1 Month</span>
+          <span className="text-white">1 Month</span> Account
         </p>
+      </div>
+
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white">
+          The Next <br className="sm:block hidden" />{" "}
+          <span className="text-gradient">Generation</span> <br />
+          Payment Method
+        </h1>
       </div>
     </div>
   </section>
